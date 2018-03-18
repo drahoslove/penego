ba685ac9f1fb772a4c9ae8e18b31c4537e8860a4
Drahoslav Bednar <drahoslav@yo2.cz>
2018-03-18 18:45:59 +0100

    Fix linux dialog by hack

---
+++ fix_linux.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"time"
+	_ "runtime"
+	"github.com/mattn/go-gtk/gtk"
+
+)
+
+// hack to make dialog work under linux
+func init() {
+	var dlg *gtk.MessageDialog
+	dlg = gtk.NewMessageDialog(nil, 0, gtk.MESSAGE_OTHER, gtk.BUTTONS_NONE, "Loading Penego . . .") // this is a lie!
+	go func() {
+		time.Sleep(time.Second)
+		dlg.Response(gtk.RESPONSE_NONE)
+	}()
+	dlg.Run()
+	dlg.Destroy()
+	for gtk.EventsPending() {
+		gtk.MainIteration()
+	}
+}
