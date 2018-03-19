34afdb86d00c31c8519a0a14aaac110376f0c7d3
Drahoslav <drahoslav@yo2.cz>
2018-03-19 14:48:54 +0100

    WIP - recreation of window - does fail

--- tools.go
+++ tools.go
@@ -1,42 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"github.com/andlabs/ui"
 )
 
 var toolWindow *ui.Window
 
-func init() {
+func tools() {
 	go func() {
+		if toolWindow != nil {
+			c := make(chan bool)
+			ui.QueueMain(func() {
+				toolWindow.Destroy()
+				c <- true
+			})
+			<- c // wait for destroy
+		}
 		err := ui.Main(func() {
 			input := ui.NewEntry()
-			button := ui.NewButton("Greet")
 			greeting := ui.NewLabel("")
+			
+			slider := ui.NewSlider(-10, 10)
+			slider.SetValue(0)
+			slider.OnChanged(func(slider *ui.Slider) {
+				fmt.Println("value", slider.Value())
+			})
+
+			button := ui.NewButton("Greet")
+			button.OnClicked(func(*ui.Button) {
+				greeting.SetText("Hello, " + input.Text() + "!")
+			})
+			
 			box := ui.NewVerticalBox()
 			box.Append(ui.NewLabel("Enter your name:"), false)
 			box.Append(input, false)
 			box.Append(button, false)
 			box.Append(greeting, false)
+			box.Append(slider, false)
+
 			toolWindow = ui.NewWindow("Hello", 200, 100, false)
 			toolWindow.SetMargined(true)
 			toolWindow.SetChild(box)
-			button.OnClicked(func(*ui.Button) {
-				greeting.SetText("Hello, " + input.Text() + "!")
-			})
 			toolWindow.OnClosing(func(*ui.Window) bool {
-				toolWindow.Hide()
-				return false
+				return true
 			})
-
+			toolWindow.Show()
 		})
 		if err != nil {
 			panic(err)
 		}
 	}()
-}
-
-func tools() {
-	toolWindow.Show()
 }
 
 func toolsLoadFile(cb func(string)) {
